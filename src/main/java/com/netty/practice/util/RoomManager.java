eb04976b0ffb883c23ea42f9e048321f95f2add2
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-23 22:15:11 +0900

    fix: RoomManager코드 동기화 자료구조로 변경

--- src/main/java/com/netty/practice/util/RoomManager.java
+++ src/main/java/com/netty/practice/util/RoomManager.java
@@ -1,20 +1,30 @@
 package com.netty.practice.util;
 
 import com.netty.practice.service.RoomService;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelId;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Component
 public class RoomManager {
     private final Map<String, ChannelGroup> rooms=new ConcurrentHashMap<>();
 
-    public Map<String, ChannelGroup> getRoomManager(){
+    public Map<String,ChannelGroup> getRoomManager(){
         return rooms;
     }
 }
