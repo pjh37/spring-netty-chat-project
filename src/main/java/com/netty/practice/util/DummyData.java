c2d91e98881c9fd1c4d5493bdd20bed3548ee2fa
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-03 02:01:26 +0900

    feat: dummydata 추가

--- src/main/java/com/netty/practice/util/DummyData.java
+++ src/main/java/com/netty/practice/util/DummyData.java
@@ -3,6 +3,8 @@ package com.netty.practice.util;
 import com.netty.practice.domain.Room;
 import com.netty.practice.repository.RoomRepository;
 import com.netty.practice.service.RoomService;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +14,14 @@ import javax.annotation.PostConstruct;
 @Component
 public class DummyData {
     private final RoomRepository roomRepository;
-
+    private final RoomManager roomManager;
     @PostConstruct
     public void init(){
         for(int i=0;i<107;i++){
-            roomRepository.save(Room.builder()
+            Room room=roomRepository.save(Room.builder()
                     .title("채팅방 리스트 "+(i+1))
                     .build());
+            roomManager.getRoomManager().put(room.getRoomId(),new DefaultChannelGroup(GlobalEventExecutor.INSTANCE));
         }
     }
 }
