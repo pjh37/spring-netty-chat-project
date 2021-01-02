0c8746d9326cb1b14e309e5792d54b510b438585
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-03 02:00:29 +0900

    feat: github 로그인 연동 성공지 핸들러 추가

--- src/main/java/com/netty/practice/config/auth/GithubAuthenticationSuccessHandler.java
+++ src/main/java/com/netty/practice/config/auth/GithubAuthenticationSuccessHandler.java
@@ -5,6 +5,7 @@ import com.netty.practice.domain.User.SessionUser;
 import com.netty.practice.domain.User.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.Principal;
 
 @Component
 @RequiredArgsConstructor
@@ -25,11 +27,7 @@ public class GithubAuthenticationSuccessHandler implements AuthenticationSuccess
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        //SessionUser sessionUser=objectMapper.convertValue(authentication.getPrincipal(),SessionUser.class);
-
-       // UserAccount userAccount=(UserAccount)authentication.getPrincipal();
-
-        //System.out.println("onAuthenticationSuccess: "+userDetails.getUsername());
+        Principal principal=(Principal)authentication.getPrincipal();
 
         response.addCookie(new Cookie("name",authentication.getName()));
         response.sendRedirect("http://localhost:3000/login/oauth2/code/github");
